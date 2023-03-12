e08c1b83e4db1069f8fd0e9b839fd4cb6a8258f4
Ena Jenkins <enajenkins@gmail.com>
2023-03-12 22:24:09 +0000

    testing an edit...

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # enajenkins.github.io
+
+## Steps to deploy a Gatsby site to GitHub Pages:
+
+1. Create a new repository on GitHub. Name the repository `"yourusername.github.io"` (replace `"yourusername"` with your GitHub username).
+
+2. Install Gatsby by running `npm install -g gatsby-cli` in your terminal.
+
+3. Create a new Gatsby site by running `gatsby new my-gatsby-site` in your terminal (replace `"my-gatsby-site"` with the name of your site).
+
+4. Change into the new directory by running `cd my-gatsby-site`.
+
+5. Edit the `gatsby-config.js` file to add your site metadata and plugins.
+
+6. Build your site by running `gatsby build`.
+
+7. Create a new branch in your repository by running `git checkout -b gh-pages`.
+
+8. Move the built files to the new branch by running `git add public && git commit -m "Initial commit"`.
+
+9. Push the new branch to GitHub by running `git push --set-upstream origin gh-pages`.
+
+10. Navigate to the `Settings` tab in your repository.
+
+11. Scroll down to the `GitHub Pages` section.
+
+12. Select `"gh-pages"` as your source branch.
+
+13. Click `"Save"` to publish your website.
+
+14. Wait a few minutes for GitHub to build your site, then navigate to `"yourusername.github.io"` in your web browser to view your website.
---
+++ ejio-blog/gatsby-config.js
@@ -0,0 +1,43 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `EJIO Blog`,
+    description: `Documentation, tutorials, and product information for EJIO projects. Code snippets, thoughts, and learnings on Front-End and Full-Stack topics including MERN, Accessibility, UX, Cloud, Cybersecurity, Interview Topics, Computer Science, and anything else I learn or find interesting along my journey`,
+    author: `Ena Jenkins`,
+    siteUrl: `https://enajenkins.github.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
